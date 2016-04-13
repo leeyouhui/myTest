5196fe69139c45625ce817cc34b125b10c42fcd3
G <G@liyouhui-PC>
2016-04-13 16:20:13 +0800

    容器。。

---
+++ src/collection/Test2.java
@@ -0,0 +1,14 @@
+package collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		System.out.println(map.get("abc"));
+
+	}
+
+}
